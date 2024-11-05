a69d4f55e8a40f2c7e93033d8f41094ca0db9764
ngnishokkrishna <ngnishokkrishna@gmail.com>
2024-11-05 21:11:11 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,8 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Hii, This is Nishok Krishna"); 
+        System.out.println("Hello, This is Murugar");
     }
 }
